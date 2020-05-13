7af952a733cd0003117910559b835455faaa83d7
PedroAcevedo <pdacevedo@uninorte.edu.co>
2020-05-13 17:39:10 -0500

    letter soup mobile

--- js/letterSoup.js
+++ js/letterSoup.js
@@ -30,12 +30,12 @@ fetch('https://incities-interactive.herokuapp.com/api/getInteractive/15')//'http
     </div>
     `;
     var words = '<h3 class="text-center"> Palabras a buscar: </h3> <div class="d-flex flex-row flex-wrap">';
-    
+
     wordList.forEach(function (word, index) {
       words += `
         <div id="${word[0]}-${index}"class="card card-tile" >
 
-        <h5 class="card-title" style="font-size: 15px;letter-spacing: 3px;">${word[0] + '_'.repeat(word.length - 1) + ' '}</h5>
+        <h5 class="card-title" >${word[0] + '_'.repeat(word.length - 1) + ' '}</h5>
                 
         </div>       
         `;
@@ -50,10 +50,10 @@ fetch('https://incities-interactive.herokuapp.com/api/getInteractive/15')//'http
     buildBoard(size, soup);
     document.querySelector("#loader").style.display = "none";
     document.getElementById('btn-back').addEventListener('click', postToServer);
-  
+
 
     $('.letters')
-    .on('mouseup', function () {
+      .on('mouseup touchstart', function () {
         selection = false;
         initial, second = undefined;
         if ($(this) != initial) {
@@ -63,7 +63,7 @@ fetch('https://incities-interactive.herokuapp.com/api/getInteractive/15')//'http
             let card = document.getElementById(selectWord[0] + '-' + index)
             card.classList.add("bg-success");
             soup_answer++;
-            if(soup_answer==wordList.length){
+            if (soup_answer == wordList.length) {
               setInterval(postToServer(), 4000);
             }
             card.innerHTML = selectWord;
@@ -79,7 +79,7 @@ fetch('https://incities-interactive.herokuapp.com/api/getInteractive/15')//'http
           selectedList = [];
         }
       })
-      .on('mousedown', function () {
+      .on('mousedown touchend', function () {
         selection = true;
         initial = $(this);
         selectedList.push(initial);
@@ -87,12 +87,99 @@ fetch('https://incities-interactive.herokuapp.com/api/getInteractive/15')//'http
         $(this).css("background-color", 'yellow');
       });
 
+    $(".unselectable ").on('mouseenter', function () {
+      //stuff to do on mouse enter
+      if (selection) {
+        if ($(this) != initial) {
+          if (!selectedList.includes($(this))) {
+            if (second == undefined) {
+              second = $(this);
+              console.log(initial.data('row'));
+              if (initial.data('row') == second.data('row')) {
+                orientation = 0;
+                if (initial.data('column') < second.data('column')) {
+                  direction = 1;
+                } else {
+                  direction = 2;
+                }
+              } else {
+                if (initial.data('column') == second.data('column')) {
+                  orientation = 1;
+                  if (initial.data('row') < second.data('row')) {
+                    direction = 1;
+                  } else {
+                    direction = 2;
+                  }
+                } else {
+                  orientation = 2;
+                  if (initial.data('column') < second.data('column')) {
+                    direction = 1;
+                  } else {
+                    direction = 2;
+                  }
+                }
+              }
+              selectBox(second);
+            } else {
+              if (orientation == 0) {
+                if (initial.data('row') == $(this).data('row')) {
+                  if (direction == 1) {
+                    if (initial.data('column') < $(this).data('column')) {
+                      selectBox($(this));
+                    }
+                  } else {
+                    if (initial.data('column') > $(this).data('column')) {
+                      selectBox($(this));
+                    }
+                  }
+                }
+              } else {
+                if (orientation == 1) {
+                  if (initial.data('column') == $(this).data('column')) {
+                    if (direction == 1) {
+                      if (initial.data('row') < $(this).data('row')) {
+                        selectBox($(this));
+                      }
+                    } else {
+                      if (initial.data('row') > $(this).data('row')) {
+                        selectBox($(this));
+                      }
+                    }
+                  }
+                } else {
+                  if (Math.abs(selectedList.slice(-1)[0].data('row') - $(this).data('row')) == 1 && Math.abs(selectedList.slice(-1)[0].data('column') - $(this).data('column')) == 1) {
+                    if (direction == 1) {
+                      if (initial.data('column') < $(this).data('column')) {
+                        selectBox($(this));
+                      }
+                    } else {
+                      if (initial.data('column') > $(this).data('column')) {
+                        selectBox($(this));
+                      }
+                    }
+                  }
+                }
+              }
+
+            }
+          } else {
+            console.log('in');
+            selectWord = selectWord.slice(0, -1);
+            selectedList.remove($(this));
+          }
+        } else {
+          second = undefined;
+        }
+      }
+    });
+
+
   });
 
 function buildBoard(size, soup) {
   var result = '<div class="wrapper" align="right" style="grid-template-columns: repeat(' + size + ', 1fr);">';
-  result = 
-  `<div class="row d-flex justify-content-between mb-3">
+  result =
+    `<div class="row d-flex justify-content-between mb-3">
   <div class="col-5 d-flex justify-content-start">
       <button id="btn-back" class="btn btn-back float-sm-right">  REGRESAR  < </button>
   </div>
@@ -117,97 +204,6 @@ function buildBoard(size, soup) {
 function verify(wordselected) {
   return wordList.findIndex((element) => element == wordselected);
 }
-
-$(document).on({
-  mouseenter: function () {
-    //stuff to do on mouse enter
-    if (selection) {
-      if ($(this) != initial) {
-        if (!selectedList.includes($(this))) {
-          if (second == undefined) {
-            second = $(this);
-            console.log(initial.data('row'));
-            if (initial.data('row') == second.data('row')) {
-              orientation = 0;
-              if (initial.data('column') < second.data('column')) {
-                direction = 1;
-              } else {
-                direction = 2;
-              }
-            } else {
-              if (initial.data('column') == second.data('column')) {
-                orientation = 1;
-                if (initial.data('row') < second.data('row')) {
-                  direction = 1;
-                } else {
-                  direction = 2;
-                }
-              } else {
-                orientation = 2;
-                if (initial.data('column') < second.data('column')) {
-                  direction = 1;
-                } else {
-                  direction = 2;
-                }
-              }
-            }
-            selectBox(second);
-          } else {
-            if (orientation == 0) {
-              if (initial.data('row') == $(this).data('row')) {
-                if (direction == 1) {
-                  if (initial.data('column') < $(this).data('column')) {
-                    selectBox($(this));
-                  }
-                } else {
-                  if (initial.data('column') > $(this).data('column')) {
-                    selectBox($(this));
-                  }
-                }
-              }
-            } else {
-              if (orientation == 1) {
-                if (initial.data('column') == $(this).data('column')) {
-                  if (direction == 1) {
-                    if (initial.data('row') < $(this).data('row')) {
-                      selectBox($(this));
-                    }
-                  } else {
-                    if (initial.data('row') > $(this).data('row')) {
-                      selectBox($(this));
-                    }
-                  }
-                }
-              } else {
-                if (Math.abs(selectedList.slice(-1)[0].data('row') - $(this).data('row')) == 1 && Math.abs(selectedList.slice(-1)[0].data('column') - $(this).data('column')) == 1) {
-                  if (direction == 1) {
-                    if (initial.data('column') < $(this).data('column')) {
-                      selectBox($(this));
-                    }
-                  } else {
-                    if (initial.data('column') > $(this).data('column')) {
-                      selectBox($(this));
-                    }
-                  }
-                }
-              }
-            }
-
-          }
-        } else {
-          console.log('in');
-          selectWord = selectWord.slice(0, -1);
-          selectedList.remove($(this));
-        }
-      } else {
-        second = undefined;
-      }
-    }
-  },
-  mouseleave: function () {
-    //stuff to do on mouse leave
-  }
-}, ".unselectable");
 
 function selectBox(element) {
   selectedList.push(element);
@@ -240,15 +236,15 @@ function postToServer() {
       console.log(res)
       console.log('Success:', res);
       let nofind = '<ul>';
-      document.querySelectorAll('.card-tile').forEach(function (card){
-        if(!card.classList.contains('bg-success')){
+      document.querySelectorAll('.card-tile').forEach(function (card) {
+        if (!card.classList.contains('bg-success')) {
           nofind += `<li>${wordList[parseInt(card.id.split('-')[1])]}</li>`;
         }
       });
       nofind += '</ul>';
       document.querySelector('.modal-title').innerHTML = "Resultados";
       document.getElementById('modal-button').innerHTML = "Terminar";
-      document.getElementById('modal-button').addEventListener('click',function(){window.location='index.html'});
+      document.getElementById('modal-button').addEventListener('click', function () { window.location = 'index.html' });
       document.getElementById('score').innerHTML = `<ul> <li>Tiempo: ${time}</li> <li>Palabras encontradas: ${res['data']['solved']}/${wordList.length}</li> <li> Palabras sin descubir: ${nofind} </li></ul>`;
       $('#myModal').modal('toggle');
     });
